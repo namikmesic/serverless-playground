1f8a76783d817f2c619e6af340d3a81d3fb2d367
Namik Mesic <namikmesic@gmail.com>
2023-05-05 09:22:46 +0200

    Restructure folders

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sam build
-
-sam deploy --stack-name namik-chat-app --region us-west-2 --parameter-overrides 'TableName=simplechat_connections' --capabilities CAPABILITY_NAMED_IAM --on-failure ROLLBACK  --config-env default --confirm-changeset
